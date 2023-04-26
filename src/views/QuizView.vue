e5767705d1a386425e27659d8c11c4c78edea69c
Сергей Брайчук <sergejbrajcuk@192.168.0.20>
2023-04-26 22:40:25 +0300

    added component result.vue, added numberOfCorrectAnswer, added function onOptionSelected

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,78 +1,51 @@
 <script setup>
-import {defineProps} from 'vue'
+import Question from '../components/Question.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import Result from '../components/Result.vue';
+import { useRoute } from 'vue-router';
+import { ref, computed} from 'vue';
+import quizes from '../data/quizes.json';
+
+const route = useRoute();
+const quizId = parseInt( route.params.id );
+const quiz = quizes.find( q => q.id === quizId );
+const currentQuestionIndex = ref( 0 );
+const numberOfCorrectAnswer = ref( 0 );
+const questionStatus = computed( () => `${ currentQuestionIndex.value } / ${ quiz.questions.length }` );
+const barPrecent = computed( () => `${ currentQuestionIndex.value / quiz.questions.length * 100 }%` );
+const showResult = ref(false)
+
+const onOptionSelected = (isCorrect) =>
+{
+  if ( isCorrect )
+  {
+    numberOfCorrectAnswer.value++
+  }
+  if ( quiz.questions.length - 1 === currentQuestionIndex.value )
+  {
+    showResult.value = true
+  }
+  currentQuestionIndex.value++
+}
 </script>
 
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="complition">
-
-        </div>
-      </div>
-    </header>
+    <QuizHeader 
+      :questionStatus="questionStatus"
+      :barPrecent="barPrecent"
+    />
     <div>
-      <div class="question-container">
-        <h1 class="question">
-          What is the chemical value of table salt
-        </h1>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
+      <Question 
+        v-if="!showResult"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result 
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswer="numberOfCorrectAnswer"
+        v-else
+      />
     </div>
   </div>
 </template>
-
-<style scoped>
-  header  {
-    margin-top: 20px;
-
-  }
-  header h4 {
-    font-size: 30px;
-  }
-  .bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-  }
-  .complition {
-    height: 100%;
-    width: 0%;
-    background-color: bisque;
-  }
-  .options-container  {
-    margin-top: 20px;
-  }
-  .question  {
-    font-size: 40px;
-    margin-bottom: 20px;
-  }
-  .option {
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-  }
-  .option-label {
-    background-color: bisque;
-    width: 50px;
-    height: 50px;
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .option-value {
-    background-color: rgb(244,239,239);
-    width: 100%;
-    font-size: 30px;
-    padding: 0 20px;
-  }
-</style>
