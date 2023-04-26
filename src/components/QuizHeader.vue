c5e32105fe0cecf7a1e17eae056ca3445c8da4cf
Сергей Брайчук <sergejbrajcuk@192.168.0.20>
2023-04-26 22:42:57 +0300

    add procent bar style

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const {
+  questionStatus,
+  barPrecent
+  } = defineProps( [
+    'questionStatus',
+    'barPrecent'
+  ] );
+</script>
+<template>
+  <header>
+    <h4>Question {{ questionStatus }}</h4>
+    <div class="bar">
+      <div class="complition" :style="{width: barPrecent }">
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header  {
+  margin-top: 20px;
+
+  }
+  header h4 {
+    font-size: 30px;
+  }
+  .bar {
+    width: 300px;
+    height: 50px;
+    border: 3px solid bisque;
+  }
+  .complition {
+    height: 100%;
+    width: 0%;
+    background-color: bisque;
+  }
+</style>
