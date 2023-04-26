0095f1e120828781652bd5e8e6a93a78b49f70cc
Сергей Брайчук <sergejbrajcuk@192.168.0.20>
2023-04-26 22:44:06 +0300

    add layout result

---
+++ src/components/Result.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { defineProps } from 'vue'
+const { quizQuestionLength, numberOfCorrectAnswer } = defineProps( [ 'quizQuestionLength', 'numberOfCorrectAnswer' ] );
+
+</script>
+<template>
+  <div class="results">
+    <p>Your results...</p>
+    <h1>{{ numberOfCorrectAnswer  + '/' + quizQuestionLength }}</h1>
+    <RouterLink to="/">Go to Back</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+  .results {
+    text-align: center;
+    padding: 100px 0;
+  }
+  p {
+    font-size: 25px;
+  }
+  h1 {
+    font-size: 80px;
+  }
+</style>
